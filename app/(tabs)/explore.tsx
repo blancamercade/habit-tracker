8ee01f36d01f1c0eac7f147977defc29027ee250
blancamercade <145469413+blancamercade@users.noreply.github.com>
2025-02-06 10:43:40 +0800

    Update explore.tsx

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, FlatList, StyleSheet, ScrollView, Button, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
