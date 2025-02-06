6aa2e9750fc9325bc35d05ba1b93f1534f3c9e37
blancamercade <145469413+blancamercade@users.noreply.github.com>
2025-02-06 10:44:52 +0800

    Update index.tsx correct error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
@@ -30,7 +30,7 @@ export default function HomeScreen() {
       };
   
       loadData();
-    }, [])
+    }, [setHabits])
   );
   
    const debugStorage = async () => {
