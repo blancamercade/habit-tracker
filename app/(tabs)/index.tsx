db5b756bcd8618f45dea4a734c2e42821804bd4d
blancamercade <145469413+blancamercade@users.noreply.github.com>
2025-02-25 19:10:09 +0800

    Update index.tsx remove styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -115,51 +115,10 @@ const logAndResetHabits = async () => {
           </TouchableOpacity>
         )}
       />
-      <TouchableOpacity style={styles.logButton} onPress={logAndResetHabits}>
-        <Text style={styles.logButtonText}>Log & Reset for tomorrow</Text>
+      <TouchableOpacity style={styles.GreenButton} onPress={logAndResetHabits}>
+        <Text style={styles.GreenButtonText}>Log & Reset for tomorrow</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingTop: 50,
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  habitItem: {
-    backgroundColor: 'white',
-    padding: 15,
-    marginVertical: 5,
-    borderRadius: 10,
-  },
-  habitCompleted: {
-    backgroundColor: '#c8e6c9',
-  },
-  habitText: {
-    fontSize: 18,
-  },
-  habitTextCompleted: {
-    textDecorationLine: 'line-through',
-    color: 'gray',
-  },
-  logButton: {
-    backgroundColor: "#1B5E20",
-    padding: 12,
-    borderRadius: 10,
-    alignItems: "center",
-  },
-  logButtonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
